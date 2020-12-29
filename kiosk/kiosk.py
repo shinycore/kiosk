ef63a5c36eb89dcc6349fc5eb876a5c06c1e2d45
shinycore <72306832+shinycore@users.noreply.github.com>
2020-12-30 00:00:00 +0100

    Use keypad to edit NumericProperty of price

--- kiosk/kiosk.py
+++ kiosk/kiosk.py
@@ -1,5 +1,6 @@
 from kivy import Config
 from kivy.app import App
+from kivy.properties import NumericProperty
 from kivy.uix.button import Button
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.togglebutton import ToggleButton
@@ -9,13 +10,31 @@ Config.set("graphics", "height", 320)
 
 
 class KioskApp(App):
+    price = NumericProperty()
+
+    def _add_price_char(self, char: str):
+        try:
+            self.price = int(f"{self.price}{char}")
+        except ValueError:
+            return
+
+    def _delete_price_char(self):
+        try:
+            self.price = int(f"0{self.price}"[:-1])
+        except ValueError:
+            return
+
     def build(self):
         price_keypad: GridLayout = self.root.ids.price_keypad
 
         for char in "7890456.123":
-            price_keypad.add_widget(Button(text=char))
+            button = Button(text=char)
+            button.on_press = lambda char_copy=char: self._add_price_char(char_copy)
+            price_keypad.add_widget(button)
 
-        price_keypad.add_widget(Button(text="Backspace"))
+        button = Button(text="Backspace")
+        button.on_press = lambda: self._delete_price_char()
+        price_keypad.add_widget(button)
 
         products_keypad: GridLayout = self.root.ids.products_keypad
 
