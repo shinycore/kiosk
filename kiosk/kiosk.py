e004e14a7361b6d22d936f9f2402851d04987ca5
shinycore <72306832+shinycore@users.noreply.github.com>
2020-12-30 00:00:00 +0100

    Add carousel and products keypad

--- kiosk/kiosk.py
+++ kiosk/kiosk.py
@@ -2,6 +2,7 @@ from kivy import Config
 from kivy.app import App
 from kivy.uix.button import Button
 from kivy.uix.gridlayout import GridLayout
+from kivy.uix.togglebutton import ToggleButton
 
 Config.set("graphics", "width", 480)
 Config.set("graphics", "height", 320)
@@ -15,3 +16,8 @@ class KioskApp(App):
             price_keypad.add_widget(Button(text=char))
 
         price_keypad.add_widget(Button(text="Backspace"))
+
+        products_keypad: GridLayout = self.root.ids.products_keypad
+
+        for p in range(6):
+            products_keypad.add_widget(ToggleButton(text=f"Product {p + 1}"))
