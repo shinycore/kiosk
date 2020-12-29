32ea70bd6a97349fca16fc74bbc994aedae3c19f
shinycore <72306832+shinycore@users.noreply.github.com>
2020-12-30 00:00:00 +0100

    Add top bar and price keypad

--- kiosk/kiosk.py
+++ kiosk/kiosk.py
@@ -1,6 +1,7 @@
 from kivy import Config
 from kivy.app import App
-from kivy.uix.label import Label
+from kivy.uix.button import Button
+from kivy.uix.gridlayout import GridLayout
 
 Config.set("graphics", "width", 480)
 Config.set("graphics", "height", 320)
@@ -8,4 +9,9 @@ Config.set("graphics", "height", 320)
 
 class KioskApp(App):
     def build(self):
-        return Label(text="Hi")
+        price_keypad: GridLayout = self.root.ids.price_keypad
+
+        for char in "7890456.123":
+            price_keypad.add_widget(Button(text=char))
+
+        price_keypad.add_widget(Button(text="Backspace"))
