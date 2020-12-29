e431656806f3a923bf67143130ddedd8d44c5ae7
shinycore <72306832+shinycore@users.noreply.github.com>
2020-12-30 00:00:00 +0100

    Use keypad to edit DictProperty of product IDs

--- kiosk/kiosk.py
+++ kiosk/kiosk.py
@@ -1,6 +1,6 @@
 from kivy import Config
 from kivy.app import App
-from kivy.properties import NumericProperty
+from kivy.properties import DictProperty, NumericProperty
 from kivy.uix.button import Button
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.togglebutton import ToggleButton
@@ -11,6 +11,7 @@ Config.set("graphics", "height", 320)
 
 class KioskApp(App):
     price = NumericProperty()
+    product_ids = DictProperty()  # there's no SetProperty, this is the closest match
 
     def _add_price_char(self, char: str):
         try:
@@ -23,6 +24,15 @@ class KioskApp(App):
             self.price = int(f"0{self.price}"[:-1])
         except ValueError:
             return
+
+    def _toggle_product_id(self, id_: int, button: ToggleButton):
+        if button.state == "normal":
+            try:
+                del self.product_ids[id_]
+            except KeyError:
+                pass
+        elif button.state == "down":
+            self.product_ids[id_] = None
 
     def build(self):
         price_keypad: GridLayout = self.root.ids.price_keypad
@@ -38,5 +48,7 @@ class KioskApp(App):
 
         products_keypad: GridLayout = self.root.ids.products_keypad
 
-        for p in range(6):
-            products_keypad.add_widget(ToggleButton(text=f"Product {p + 1}"))
+        for id_ in range(6):
+            button = ToggleButton(text=f"Product {id_ + 1}")
+            button.on_press = lambda id_copy=id_, button_copy=button: self._toggle_product_id(id_copy, button_copy)
+            products_keypad.add_widget(button)
