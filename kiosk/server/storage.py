0460c486a322927d8ad4126ddfb834d2e5d70220
shinycore <72306832+shinycore@users.noreply.github.com>
2020-12-30 00:00:00 +0100

    Store product IDs as a single integer

--- kiosk/server/storage.py
+++ kiosk/server/storage.py
@@ -1,7 +1,16 @@
 import csv
 from dataclasses import dataclass, field
 from datetime import datetime
+from functools import reduce
 from typing import List
+
+
+def _id_list_to_int(list_: List[int]) -> int:
+    return reduce(lambda res, id_: res ^ (1 << id_), list_, 0)
+
+
+def _int_to_id_list(int_: int) -> List[int]:
+    return [id_ for id_ in range(int_.bit_length()) if int_ & (1 << id_)]
 
 
 @dataclass
@@ -25,7 +34,9 @@ class Storage:
         try:
             with open(self._filename, "r") as f:
                 self.data = [
-                    StorageEntry(date=datetime.fromtimestamp(int(q[0])), price=int(q[1]), product_ids=q[2])
+                    StorageEntry(
+                        date=datetime.fromtimestamp(int(q[0])), price=int(q[1]), product_ids=_int_to_id_list(int(q[2]))
+                    )
                     for q in csv.reader(f)
                 ]
         except FileNotFoundError:
@@ -33,7 +44,9 @@ class Storage:
 
     def _write(self):
         with open(self._filename, "w") as f:
-            csv.writer(f).writerows([(int(q.date.timestamp()), q.price, q.product_ids) for q in self.data])
+            csv.writer(f).writerows(
+                [(int(q.date.timestamp()), q.price, _id_list_to_int(q.product_ids)) for q in self.data]
+            )
 
     def append(self, price: int, product_ids: List[int]):
         self.data.append(StorageEntry(price, product_ids))
