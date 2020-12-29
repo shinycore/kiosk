5c3675bfaf86fb9c3ffd4dda1d844cccade8d1a6
shinycore <72306832+shinycore@users.noreply.github.com>
2020-12-30 00:00:00 +0100

    Add StorageEntry dataclass

--- kiosk/server/storage.py
+++ kiosk/server/storage.py
@@ -1,6 +1,15 @@
 import csv
-import datetime
+from dataclasses import dataclass, field
+from datetime import datetime
 from typing import List
+
+
+@dataclass
+class StorageEntry:
+    price: int
+    product_ids: List[int]
+
+    date: datetime = field(default_factory=datetime.now)
 
 
 class Storage:
@@ -8,23 +17,24 @@ class Storage:
 
     def __init__(self, filename: str = "data.csv"):
         self._filename = filename
-        self.data = []
+        self.data: List[StorageEntry] = []
 
         self._read()
 
     def _read(self):
         try:
             with open(self._filename, "r") as f:
-                self.data = list(csv.reader(f))
+                self.data = [
+                    StorageEntry(date=datetime.fromtimestamp(int(q[0])), price=int(q[1]), product_ids=q[2])
+                    for q in csv.reader(f)
+                ]
         except FileNotFoundError:
             pass  # fresh install
 
     def _write(self):
         with open(self._filename, "w") as f:
-            csv.writer(f).writerows(self.data)
+            csv.writer(f).writerows([(int(q.date.timestamp()), q.price, q.product_ids) for q in self.data])
 
     def append(self, price: int, product_ids: List[int]):
-        timestamp = int(datetime.datetime.now().timestamp())
-
-        self.data.append((timestamp, price, product_ids))
+        self.data.append(StorageEntry(price, product_ids))
         self._write()
