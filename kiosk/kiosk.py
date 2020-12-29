2b9e1ebb9e0adfa4f2587e93e54b1d006e905e2f
shinycore <72306832+shinycore@users.noreply.github.com>
2020-12-30 00:00:00 +0100

    Change initial screen dimensions

--- kiosk/kiosk.py
+++ kiosk/kiosk.py
@@ -1,5 +1,9 @@
+from kivy import Config
 from kivy.app import App
 from kivy.uix.label import Label
+
+Config.set("graphics", "width", 480)
+Config.set("graphics", "height", 320)
 
 
 class KioskApp(App):
